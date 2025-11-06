a310c3770acb19ac30fdc55bfb0406b944072c50
Pulkit6377 <pulkitgarg6377@gmail.com>
2025-11-06 16:46:22 +0530

    Vercel Config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:"0.0.0.0",
-    port:5174
+  base: "/",  
+  server: {
+    host: "0.0.0.0",
+    port: 5174
   }
 });
